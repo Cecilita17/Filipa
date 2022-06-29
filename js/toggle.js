4b97f0d97957c447fd484978a44f5caae46c3eb2
Cecilia <ceciferreri@gmail.com>
2022-06-29 16:25:06 -0300

    añadido menu hamburguesa

--- js/toggle.js
+++ js/toggle.js
@@ -21,3 +21,16 @@ contenedorModal.addEventListener("click", ()=>{
 })
 
 //menu hamburguesa
+
+const botonHambur = document.getElementById("nav-toggle")
+const nav2 = document.getElementById("nav2")
+const menuCerrar = document.getElementById("nav-toggle-cerrar")
+
+botonHambur.addEventListener("click", ()=>{
+    nav2.classList.toggle("visible")
+    
+})
+
+menuCerrar.addEventListener("click", ()=>{
+    nav2.classList.toggle("visible")
+})
