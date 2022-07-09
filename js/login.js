0544d35041575bffbac0570214544ca2edbcf0ae
Cecilia <ceciferreri@gmail.com>
2022-07-09 15:52:54 -0300

    cambios

--- js/login.js
+++ js/login.js
@@ -1,26 +1,36 @@
 /* const formLogin = document.getElementById("form-login")
-const user = document.getElementById('userr')
-const contrasenia = document.getElementById('pass')
-const checkbox = document.getElementById('checkbox')
-const btnIngresar = document.getElementById('btnIngresar') */
+const formSignUp = document.getElementById('formSignUp') */
 
-const formSignUp = document.getElementById('formSignUp')
+
+
+
+const btnIngresar = document.getElementById('btnIngresar') 
 const btnRegistrar = document.getElementById('boton-registrar')
-
-console.log(btnRegistrar);
 
 const datos = JSON.parse(localStorage.getItem("datos")) || []; 
 
-if (btnRegistrar){
-    btnRegistrar.addEventListener("click", (e)=>{
-        e.preventDefault()
-        const crearUser = document.getElementById('crearUser').value
-        const email = document.getElementById('email').value
-        const crearPas = document.getElementById('crearPas').value
-        const rePassword = document.getElementById('rePassword').value
-        
-        crearUsuario(crearUser, email, crearPas, rePassword)
-    
+
+//Registrarse
+function validarSignup(crearUser, crearPas, rePassword){
+    if(crearUser == "" || crearPas == "" ||rePassword == ""){
+        Swal.fire(
+            'ERROR!',
+            'Los campos no deben estar vacios',
+            'error'
+          )
+    }else if(crearPas.length < 5){
+        Swal.fire(
+            'ERROR!',
+            'Contraseña debe ser mayor a 7 digitos',
+            'error'
+          )
+    }else if(crearPas !== rePassword){
+        Swal.fire(
+            'ERROR!',
+            'Las contraseñas no coinciden',
+            'error'
+          )
+    }else{
         Swal.fire({
             icon: 'success',
             title: 'Registro exitoso',
@@ -32,7 +42,6 @@ if (btnRegistrar){
             denyButtonColor: 'rgb(170, 96, 96)',
             
           }).then((result) => {
-            /* Read more about isConfirmed, isDenied below */
             if (result.isConfirmed) {
                 window.location.href ="./iniSesion.html"
             } else if (result.isDenied) {
@@ -40,6 +49,25 @@ if (btnRegistrar){
             }
             
         })
+    }
+
+    
+    
+}
+
+if (btnRegistrar){
+    btnRegistrar.addEventListener("click", (e)=>{
+        e.preventDefault()
+        const crearUser = document.getElementById('crearUser').value
+        const email = document.getElementById('email').value
+        const crearPas = document.getElementById('crearPas').value
+        const rePassword = document.getElementById('rePassword').value
+        
+        crearUsuario(crearUser, email, crearPas, rePassword)
+
+        validarSignup(crearUser, crearPas, rePassword)
+    
+        
     })
 }
 
@@ -55,14 +83,67 @@ class NewUser{
 }
 
 function crearUsuario (crearUser, email, crearPas, rePassword ){
-    localStorage.setItem("dato", JSON.stringify(new NewUser(crearUser, email, crearPas, rePassword )))
-    
-    /* document.getElementById('crearUser').value = ""
-    document.getElementById('email').value= ""
-    document.getElementById('crearPas').value= ""
-    document.getElementById('rePassword').value = "" */
+    /* datos.push(localStorage.setItem("dato", JSON.stringify(new NewUser(crearUser, email, crearPas, rePassword )))) */
+
+    datos.push(new NewUser(crearUser, email, crearPas, rePassword))
+    localStorage.setItem("dato", JSON.stringify(datos))
+
+    /* localStorage.setItem("dato", JSON.stringify(new NewUser(crearUser, email, crearPas, rePassword ))) */
+
+    console.log(datos);
 }
 
 
+//Iniciar sesion
+let recuperarLS = JSON.parse(localStorage.getItem('dato'))
+const user = document.getElementById('userr')
+const pass = document.getElementById('pass')
+
+function validarLogin(user, pass){
+    
+    if(user.value == recuperarLS[0].usuario && pass.value == recuperarLS[0].contrasenia){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'inicio de sesion exitoso',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    
+    }else{
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Usuario o contraseña incorrectos',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }
+}
+
+function save() {	
+	const checkbox = document.getElementById('checkbox')
+    localStorage.setItem("checkbox", checkbox.checked);	
+}
+
+function rememberMe(){
+    if (checkbox.checked && user.value !== ""){
+    recuperarLS[0].usuario = user.value;
+    localStorage.checkbox = checkbox.value;
+}else{
+    recuperarLS[0].usuario = "";
+    recuperarLS[0].contrasenia = "";
+}}
 
 
+if(btnIngresar){
+    btnIngresar.addEventListener("click", (e)=> {
+        e.preventDefault()
+        validarLogin(user, pass)
+        rememberMe()
+        
+    })
+    
+}
+
+console.log(recuperarLS);
