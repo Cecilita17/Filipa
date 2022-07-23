b5a5dfe9825d44ee2ba25fd5dfd19e1b1fcd6bef
Cecilia <ceciferreri@gmail.com>
2022-07-23 12:13:50 -0300

    cambios

--- bd/bd.js
+++ bd/bd.js
@@ -1,7 +1,5 @@
-
-
 export const traerDatos = async() => {
-    let respuesta = await fetch("../bd/stock.json" || "./bd/stock.json")
+    let respuesta = await fetch("./bd/stock.json" || "../bd/stock.json")
     return respuesta.json()
 }
 
