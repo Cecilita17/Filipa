e0ce92c9e62b55886069ea9ea7ebc65c46c86e83
Cecilia <ceciferreri@gmail.com>
2022-07-23 12:04:08 -0300

    correccion ruta bd

--- bd/bd.js
+++ bd/bd.js
@@ -1,5 +1,7 @@
+
+
 export const traerDatos = async() => {
-    let respuesta = await fetch(" ./../bd/stock.json")
+    let respuesta = await fetch("../bd/stock.json" || "./bd/stock.json")
     return respuesta.json()
 }
 
